5ef50d5458621375a40c483b4ed6a85c48caf423
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2017-12-03 11:20:14 -0500

    fixed tabs layout on android

--- src/Main.js
+++ src/Main.js
@@ -70,7 +70,7 @@ export default class Main extends React.Component {
   }
 
   async showIntro() {
-    let val = '102';
+    let val = '104';
     let intro = await SingleStorage.get('intro');
     if (intro !== val) {
       RouterUtil.resetTo(this.refs.navigation, 'Intro');
