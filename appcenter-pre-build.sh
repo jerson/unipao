caee2915a71ef333e83e7da406836d92eabebd4b
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2017-12-17 23:25:49 -0500

    updated prebuild

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+cd $APPCENTER_SOURCE_DIRECTORY
+echo "Config copied"
 cp Config.js-dist Config.js
-sed -i 's/SUPER_SECURE_CODE/$SUPER_SECURE_CODE/g' Config.js
-sed -i 's/AUTHOR/$AUTHOR/g' Config.js
+echo "Config loaded"
+sed -i 's/SUPER_SECURE_CODE/${SUPER_SECURE_CODE}/g' Config.js
+sed -i 's/AUTHOR/${AUTHOR}/g' Config.js
