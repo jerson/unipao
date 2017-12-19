2dc5ef33246a4e7d33e3870f982ed5dce4228224
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2017-12-18 23:15:57 -0500

    fixed double quotes

--- webpage/webpack.config.js
+++ webpage/webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
         })
     ],
     devServer: {
-        contentBase: path.join(__dirname, "dist"),
+        contentBase: path.join(__dirname, 'dist'),
         compress: true,
         port: 3003
     }
