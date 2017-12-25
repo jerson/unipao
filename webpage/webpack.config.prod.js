36de9b58c6b077ebde78dd3d9aa0bd7aa3775c4e
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2017-12-25 17:10:00 -0500

    added dns records for webpage

--- webpage/webpack.config.prod.js
+++ webpage/webpack.config.prod.js
@@ -46,6 +46,11 @@ module.exports = {
             hash:true,
             inject:false
         }),
+        new HtmlWebpackPlugin({
+            template: './CNAME',
+            filename:'CNAME',
+            inject:false
+        }),
         new ExtractTextPlugin({
             filename: 'unipao.css',
         }),
