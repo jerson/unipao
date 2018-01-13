ba97701f406238638b1344a5f87e374d7eca779c
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2018-01-13 17:31:18 -0500

    added missing files

---
+++ src/scenes/user/IntranetScreen.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Theme } from '../../themes/styles';
+import PropTypes from 'prop-types';
+import Loading from '../../components/ui/Loading';
+import { _ } from '../../modules/i18n/Translator';
+import DimensionUtil from '../../modules/util/DimensionUtil';
+import { tabsOptionsSub } from '../../routers/Tabs';
+import { TabNavigator } from 'react-navigation';
+import LevelScreen from './intranet/LevelScreen';
+
+const TAG = 'IntranetScreen';
+export default class IntranetScreen extends React.Component {
+  static contextTypes = {
+    notification: PropTypes.object.isRequired
+  };
+
+  static navigationOptions = ({ navigation, screenProps }) => ({
+    title: _('Aula Virtual'),
+    headerBackTitle: null,
+    headerTitleStyle: [Theme.title, Theme.subtitle],
+    headerTintColor: Theme.tintColor,
+    headerStyle: [Theme.navigationBar, Theme.subNavigationBar]
+  });
+
+  state = {
+    isLoading: false
+  };
+
+  render() {
+    let { isLoading } = this.state;
+    let paddingTop = DimensionUtil.getNavigationBarHeight();
+    return (
+      <View style={[styles.container, { paddingTop }]}>
+        {/*<Background/>*/}
+        {isLoading && <Loading margin />}
+        {!isLoading && <LevelsTab />}
+      </View>
+    );
+  }
+}
+
+const LevelsTab = TabNavigator(
+  {
+    UG: {
+      screen: ({ navigation, screenProps }) => {
+        return <LevelScreen level={'UG'} />;
+      },
+      navigationOptions: ({ navigation, screenProps }) => {
+        return {
+          tabBarLabel: _('Pregrado')
+        };
+      }
+    },
+    GR: {
+      screen: ({ navigation, screenProps }) => {
+        return <LevelScreen level={'GR'} />;
+      },
+      navigationOptions: ({ navigation, screenProps }) => {
+        return {
+          tabBarLabel: _('Postgrado')
+        };
+      }
+    },
+    UT: {
+      screen: ({ navigation, screenProps }) => {
+        return <LevelScreen level={'UT'} />;
+      },
+      navigationOptions: ({ navigation, screenProps }) => {
+        return {
+          tabBarLabel: _('Gente que trabaja')
+        };
+      }
+    },
+    UB: {
+      screen: ({ navigation, screenProps }) => {
+        return <LevelScreen level={'UB'} />;
+      },
+      navigationOptions: ({ navigation, screenProps }) => {
+        return {
+          tabBarLabel: _('Centro de idiomas')
+        };
+      }
+    }
+  },
+  {
+    ...tabsOptionsSub,
+    tabBarOptions: {
+      ...tabsOptionsSub.tabBarOptions,
+      scrollEnabled: true
+    }
+  }
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  }
+});
---
+++ src/scenes/user/ProfileScreen.js
@@ -0,0 +1,171 @@
+import React from 'react';
+import {
+  Alert,
+  Dimensions,
+  Image,
+  Platform,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import Icon from '../../components/ui/Icon';
+import { Theme } from '../../themes/styles';
+import PropTypes from 'prop-types';
+import Background from '../../components/ui/Background';
+import Auth from '../../modules/session/Auth';
+import Button from '../../components/ui/Button';
+import RouterUtil from '../../modules/util/RouterUtil';
+import { titleize } from 'underscore.string';
+import ImageUtil from '../../modules/util/ImageUtil';
+import { _ } from '../../modules/i18n/Translator';
+
+const TAG = 'ProfileScreen';
+export default class ProfileScreen extends React.Component {
+  static contextTypes = {
+    notification: PropTypes.object.isRequired
+  };
+
+  static navigationOptions = {
+    tabBarLabel: _('Perfíl'),
+    tabBarIcon: ({ tintColor }) => (
+      <Icon
+        name={'user'}
+        type={'FontAwesome'}
+        style={[Theme.tabTarIcon, { color: tintColor }]}
+      />
+    )
+  };
+
+  state = {};
+
+  logout = () => {
+    Alert.alert(_('Cerrar sesión'), _('¿Estas seguro?'), [
+      {
+        text: _('Cancelar'),
+        onPress: () => {},
+        style: 'cancel'
+      },
+      {
+        text: _('Cerrar sesión'),
+        onPress: async () => {
+          await Auth.logout();
+          let { screenProps } = this.props;
+          RouterUtil.resetTo(screenProps.rootNavigation, 'Login');
+        }
+      }
+    ]);
+  };
+  onDimensionsChange = () => {
+    this.forceUpdate();
+  };
+
+  componentDidMount() {
+    Dimensions.addEventListener('change', this.onDimensionsChange);
+  }
+
+  componentWillUnmount() {
+    Dimensions.removeEventListener('change', this.onDimensionsChange);
+  }
+
+  render() {
+    let minHeight = Dimensions.get('window').height;
+    let user = Auth.getUser();
+
+    return (
+      <ScrollView
+        contentContainerStyle={[styles.content, { minHeight }]}
+        style={[styles.container]}
+        keyboardShouldPersistTaps={'handled'}
+        showsVerticalScrollIndicator={true}
+      >
+        {Platform.OS === 'ios' && (
+          <StatusBar
+            backgroundColor="#0d61ac"
+            translucent
+            animated
+            barStyle="dark-content"
+          />
+        )}
+        <Background />
+
+        <View style={styles.profile}>
+          <View style={[styles.imageContainer, Theme.shadowDefault]}>
+            <Image
+              style={styles.imagePlaceholder}
+              source={require('../../images/placeholder.png')}
+            />
+            <Image
+              style={[styles.image]}
+              defaultSource={require('../../images/placeholder.png')}
+              source={{ uri: ImageUtil.getUserImage(user) }}
+            />
+          </View>
+          <View style={styles.infoContainer}>
+            <Text style={[styles.name, styles.id, Theme.textShadow]}>
+              {user.id}
+            </Text>
+            <Text style={[styles.name, Theme.textShadow]}>
+              {titleize(user.name)}
+            </Text>
+          </View>
+        </View>
+
+        <Button
+          type={'primary'}
+          onPress={this.logout}
+          label={_('Cerrar sesión')}
+          icon={'log-out'}
+          iconType={'Entypo'}
+        />
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {},
+  content: {
+    paddingTop: 20,
+    paddingBottom: 70,
+    justifyContent: 'center'
+  },
+  profile: {
+    alignItems: 'center',
+    padding: 10
+  },
+  imageContainer: {
+    width: 100,
+    height: 100,
+    borderRadius: 50
+  },
+  imagePlaceholder: {
+    position: 'absolute',
+    left: 0,
+    width: 100,
+    height: 100,
+    borderRadius: 50
+  },
+  image: {
+    backgroundColor: 'transparent',
+    width: 100,
+    height: 100,
+    borderRadius: 50
+  },
+  infoContainer: {
+    backgroundColor: 'transparent',
+    padding: 10,
+    paddingBottom: 3
+  },
+  id: {
+    fontSize: 13,
+    color: 'rgba(255,255,255,0.5)',
+    paddingBottom: 2
+  },
+  name: {
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 15
+  }
+});
