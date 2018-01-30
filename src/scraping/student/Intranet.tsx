d548878192e7773600bd0dc8e2ee905fdc8adac8
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2018-01-29 22:06:36 -0500

    added new methods for grade report in scraping

--- src/scraping/student/Intranet.tsx
+++ src/scraping/student/Intranet.tsx
@@ -35,6 +35,28 @@ export interface PeriodModel {
   name: string;
 }
 
+export interface GradeReportCourseModel {
+  cycle: string;
+  course: string;
+  description: string;
+  credits: string;
+  semester: string;
+  type: string;
+  finalGrade: string;
+  apl: string;
+  ppa: string;
+  pps: string;
+}
+export interface GradeReportModel {
+  items: GradeReportCourseModel[];
+  weightedAverageCumulative: number;
+  approvedCourses: number;
+  lastAcademicSemester: number;
+  semiannualWeightedAverage: number;
+  approvedCredits: number;
+  graduated: boolean;
+}
+
 export interface PaymentModel {
   id: string;
   receipt: string;
@@ -178,13 +200,42 @@ export default class Intranet {
     ];
   }
 
-  static async getLevelsEnrollment(): Promise<LevelModel[]> {
+  static async getLevelByLevelGrades(levelCode: string): Promise<LevelModel> {
+    let levels = await this.getLevelsGrades();
+    let name = '';
+    switch (levelCode) {
+      case 'UT':
+        name = 'PGCT';
+        break;
+      case 'UB':
+        name = 'ESTUDIOS COMPLEMENTARIOS';
+        break;
+      case 'GR':
+        name = 'POSTGRADO';
+        break;
+      case 'UG':
+        name = 'PREGRADO';
+        break;
+      default:
+        name = 'ERROR';
+        break;
+    }
+
+    for (let level of levels) {
+      if (level.name.indexOf(name) !== -1) {
+        return level;
+      }
+    }
+    throw Error('no hay level');
+  }
+
+  static async getLevelsGrades(): Promise<LevelModel[]> {
     let levels: LevelModel[] = [];
     try {
       let $ = await RequestUtil.fetch(
         '/aulavirtual.aspx?f=YAAFIMA&r=A',
         {},
-        { tag: 'Intranet.getLevelsEnrollment', checkSession: true }
+        { tag: 'Intranet.getLevelsGrades', checkSession: true }
       );
 
       let $content = $('#cbo_afima_nivel');
@@ -203,20 +254,20 @@ export default class Intranet {
         });
       });
     } catch (e) {
-      Log.info(TAG, 'getLevelsEnrollment', e);
+      Log.info(TAG, 'getLevelsGrades', e);
       throw e;
     }
 
     return levels;
   }
 
-  static async getPrograms(levelEnrollment: string): Promise<ProgramModel[]> {
+  static async getPrograms(levelGrades: string): Promise<ProgramModel[]> {
     let programs: ProgramModel[] = [];
     try {
       let params = {
         f: 'YAAFIMA',
         a: 'CTRL_CBO_PROGRAMA',
-        codigo_uno: levelEnrollment,
+        codigo_uno: levelGrades,
         codigo_dos: '_afima_'
       };
 
@@ -411,7 +462,156 @@ export default class Intranet {
     return periods;
   }
 
-  static async getCourses(period: string, level: string) {}
+  static async getGradesReport(
+    levelGrade: string,
+    program: string
+  ): Promise<GradeReportModel> {
+    let report: GradeReportModel = {
+      items: [],
+      weightedAverageCumulative: 0,
+      approvedCourses: 0,
+      lastAcademicSemester: 0,
+      semiannualWeightedAverage: 0,
+      approvedCredits: 0,
+      graduated: false
+    };
+    let $;
+    try {
+      $ = await RequestUtil.fetch(
+        '/aulavirtual.aspx?f=YAAFIMA&r=A',
+        {},
+        { tag: 'Intranet.getGradesReport', checkSession: true }
+      );
+    } catch (e) {
+      Log.info(TAG, 'getGradesReport', e);
+      throw e;
+    }
+
+    let codeScript = $('#btn_consultar').attr('onclick') || '';
+    codeScript = codeScript
+      .replace("javascript:mnuya_load_reporte_nota('_afima_',", '')
+      .replace(');', '')
+      .replace(/&apos;/g, '')
+      .replace(/'/g, '');
+
+    let params = {
+      f: 'YAAFIMA',
+      a: 'LIST_REPORTE_NOTAS',
+      codigo_uno: levelGrade,
+      codigo_dos: program,
+      codigo_tres: codeScript
+    };
+
+    try {
+      let $ = await RequestUtil.fetch(
+        '/controlador/cargador.aspx',
+        {
+          method: 'POST',
+          body: ParamsUtils.getFormData(params)
+        },
+        { tag: 'Intranet.getGradesReport', checkSession: true }
+      );
+      let $tableReport = $('table:first');
+      $('table:first tr', $tableReport).each((index, value) => {
+        let cycle = $('td:nth-child(1)', value)
+          .text()
+          .trim();
+
+        let cycleCheck = parseInt(cycle, 10);
+
+        if (cycleCheck > 0) {
+          let cycle = $('td:nth-child(1)', value)
+            .text()
+            .trim();
+          let course = $('td:nth-child(2)', value)
+            .text()
+            .trim();
+          let description = $('td:nth-child(3)', value)
+            .text()
+            .trim();
+          let credits = $('td:nth-child(4)', value)
+            .text()
+            .trim();
+          let semester = $('td:nth-child(5)', value)
+            .text()
+            .trim();
+          let type = $('td:nth-child(6)', value)
+            .text()
+            .trim();
+          let finalGrade = $('td:nth-child(7)', value)
+            .text()
+            .trim();
+          let apl = $('td:nth-child(8)', value)
+            .text()
+            .trim();
+          let ppa = $('td:nth-child(9)', value)
+            .text()
+            .trim();
+          let pps = $('td:nth-child(10)', value)
+            .text()
+            .trim();
+
+          report.items.push({
+            cycle,
+            course,
+            description,
+            credits,
+            semester,
+            type,
+            finalGrade,
+            apl,
+            ppa,
+            pps
+          });
+        }
+      });
+
+      let $tableResume = $('table:last');
+      let weightedAverageCumulative = parseFloat(
+        $('tr:nth-child(1) > td:nth-child(2)', $tableResume)
+          .text()
+          .trim()
+      );
+      let approvedCourses = parseInt(
+        $('tr:nth-child(1) > td:nth-child(4)', $tableResume)
+          .text()
+          .trim(),
+        10
+      );
+      let lastAcademicSemester = parseInt(
+        $('tr:nth-child(1) > td:nth-child(6)', $tableResume)
+          .text()
+          .trim(),
+        10
+      );
+      let semiannualWeightedAverage = parseFloat(
+        $('tr:nth-child(2) > td:nth-child(2)', $tableResume)
+          .text()
+          .trim()
+      );
+      let approvedCredits = parseInt(
+        $('tr:nth-child(2) > td:nth-child(4)', $tableResume)
+          .text()
+          .trim(),
+        10
+      );
+      let graduated =
+        $('tr:nth-child(2) > td:nth-child(4)', $tableResume)
+          .text()
+          .trim() === 'SI';
+
+      report.weightedAverageCumulative = weightedAverageCumulative;
+      report.approvedCourses = approvedCourses;
+      report.lastAcademicSemester = lastAcademicSemester;
+      report.semiannualWeightedAverage = semiannualWeightedAverage;
+      report.approvedCredits = approvedCredits;
+      report.graduated = graduated;
+    } catch (e) {
+      Log.info(TAG, 'getGradesReport', e);
+      throw e;
+    }
+    return report;
+  }
 
   private static getLevelCode($: JQueryStatic, level: string): string {
     let code = '';
