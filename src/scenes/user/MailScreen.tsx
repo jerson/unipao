5a56c39b6abee50626ca1bc11bc9677891aab712
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2018-02-05 11:08:55 -0500

    user agent web

--- src/scenes/user/MailScreen.tsx
+++ src/scenes/user/MailScreen.tsx
@@ -101,7 +101,7 @@ export default class MailScreen extends React.Component<
             uri: 'https://mail.google.com/a/upao.edu.pe',
             headers: {
               'User-Agent':
-                'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36'
+                'Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19'
             }
           }}
         />
