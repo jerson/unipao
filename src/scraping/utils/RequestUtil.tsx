49d8192287ef64d81bbca72cd7d361c6d5452aff
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2018-02-05 11:01:30 -0500

    added fake user agent

--- src/scraping/utils/RequestUtil.tsx
+++ src/scraping/utils/RequestUtil.tsx
@@ -30,6 +30,10 @@ export default class RequestUtil {
         {
           redirect: 'follow',
           credentials: 'include',
+          headers: {
+            'User-Agent':
+              'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36'
+          },
           ...params
         },
         tag
