2c93086b4f18b2f25f294eb961880f0c5bc9d55a
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2018-02-05 11:02:45 -0500

    FAKE FAKE

--- src/scraping/utils/RequestUtil.tsx
+++ src/scraping/utils/RequestUtil.tsx
@@ -32,7 +32,7 @@ export default class RequestUtil {
           credentials: 'include',
           headers: {
             'User-Agent':
-              'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36'
+              'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36'
           },
           ...params
         },
